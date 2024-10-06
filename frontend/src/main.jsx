c345bbf5928e239c8747d220652c4318403268df
shivrajlawhare <shivrajlawhare6@gmail.com>
2024-10-06 20:36:13 +0530

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from 'react'; // Import React
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
