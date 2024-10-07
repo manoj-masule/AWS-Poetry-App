8c933e5ae7a123934b801b80bcb2a662b656a20e
shivrajlawhare <shivrajlawhare6@gmail.com>
2024-10-07 13:00:25 +0530

    first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = "http://3.92.166.65:3000"; // Set your backend URL here
+const API_URL = "http://54.92.247.150:3000"; // Set your backend URL here
 
 export default API_URL;
